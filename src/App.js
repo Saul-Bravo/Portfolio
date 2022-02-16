e4329eaaa6518119f726580b56809bb2802127af
Saul-Bravo <sauluriel1402@gmail.com>
2022-02-16 15:44:30 -0700

    react project created

---
+++ src/App.js
@@ -0,0 +1,16 @@
+
+import classes from './App.module.css';
+
+function App() {
+  return (
+    <div className={classes.App}>
+      <div className={classes.Header}>
+        <div className={classes.Presentation}>
+          
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
